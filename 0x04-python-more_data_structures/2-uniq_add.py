a70bfa9c39d44b2d49376b756f5320e543b34749
CABON-TECH <kishushubonifacembogho@gmail.com>
2022-12-07 11:46:14 -0800

    uniq add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,7 @@
 def uniq_add(my_list=[]):
     uniq_list = set(my_list)
     num = 0
-    for i in uniq_list:
-        sum += 1
+    for i in uniq_lisit:
+        num += 1
 
-    return sum
+    return (num)
