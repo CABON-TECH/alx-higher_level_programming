2635741727f507903876d3a987664ea3eee1d995
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-02-15 08:47:06 -0800

    never empty

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,9 +1,2 @@
--- Write a script that creates the table force_name on your MySQL server.
-
--- force_name description: id INT, name VARCHAR(256) can’t be null
--- The database name will be passed as an argument of the mysql command
--- If the table force_name already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS force_name (
-	id INT ,
-	name VARCHAR(256) NOT NULL
-);
+-- Write a script that creates the table id_not_null on MySQL server.
+CREATE TABLE  IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
